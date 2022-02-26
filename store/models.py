7dc0c3d755ec5bf3b8c948b9a1751e9bec63f100
SeyedMohammadAli Noori <13noori79@gmail.com>
2022-02-27 00:00:05 +0330

    Code until Section 4: 4- Running Migrations

--- store/models.py
+++ store/models.py
@@ -10,13 +10,15 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True,related_name='+')
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
@@ -69,6 +71,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE,)
+    zip_code = models.CharField(max_length=255)
 
 
 class Cart(models.Model):
